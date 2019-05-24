0c309207e34984bd458b8d5ae243ce930e3fbbf7
yangbing <yangbing@camera360.com>
2019-05-24 16:18:16 +0800

    code style

--- Client/Redis/ClusterPool.go
+++ Client/Redis/ClusterPool.go
@@ -1,35 +1,35 @@
 package Redis
 
 func newClusterPool(pool *Pool) interface{} {
-	return &ClusterPool{
-		pool: pool,
-	}
+    return &ClusterPool{
+        pool: pool,
+    }
 }
 
 type ClusterPool struct {
-	pool *Pool
+    pool *Pool
 }
 
 func (c *ClusterPool) getAddrByKey(cmd, key, prevDft string) string {
-	c.pool.lock.RLock()
-	defer c.pool.lock.RUnlock()
-	return c.pool.hashRing.GetNode(key)
+    c.pool.lock.RLock()
+    defer c.pool.lock.RUnlock()
+    return c.pool.hashRing.GetNode(key)
 }
 
 // first init Cluster
 func (c *ClusterPool) startCheck() {
-	for addr, item := range c.pool.servers {
-		c.pool.hashRing.AddNode(addr, item.weight)
-	}
+    for addr, item := range c.pool.servers {
+        c.pool.hashRing.AddNode(addr, item.weight)
+    }
 }
 
 // timing check cluster
 func (c *ClusterPool) check(addr, aType string) {
-	c.pool.lock.Lock()
-	defer c.pool.lock.Unlock()
-	if aType == NodeActionAdd {
-		c.pool.hashRing.AddNode(addr, c.pool.servers[addr].weight)
-	} else if aType == NodeActionDel {
-		c.pool.hashRing.DelNode(addr)
-	}
+    c.pool.lock.Lock()
+    defer c.pool.lock.Unlock()
+    if aType == NodeActionAdd {
+        c.pool.hashRing.AddNode(addr, c.pool.servers[addr].weight)
+    } else if aType == NodeActionDel {
+        c.pool.hashRing.DelNode(addr)
+    }
 }
